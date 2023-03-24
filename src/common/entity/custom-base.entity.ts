01decd70a01fb0f3a3d35ec2618a360f23c3870e
Artem Khalturin <goxalk@gmail.com>
2023-03-24 21:45:22 +0200

    feat: create custom base entity

---
+++ src/common/entity/custom-base.entity.ts
@@ -0,0 +1,18 @@
+import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+export class CustomBaseEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
